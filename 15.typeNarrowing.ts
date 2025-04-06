1578690f1215a254650842f3b69a1e0e97ed4434
dcpasha <pavelvpotapov8@gmail.com>
2025-04-06 13:18:05 -0400

    Add more info on type narrowing

--- 15.typeNarrowing.ts
+++ 15.typeNarrowing.ts
@@ -67,5 +67,122 @@ console.log(showDur)
 
 //////////////////////////////////////////////////////////////////
 /* instaceof Narrowing 
-instanceof is a JS operator that allows us to cheeck if one thing is an instance of another
+instanceof is a JS operator that allows us to check if one thing is an instance of another
+on the prototype chain
+
+Works for things that we instantiate with 'new' 
 */
+
+function printFullDate(date: string | Date) {
+    if (date instanceof Date) {
+        console.log(date.toUTCString)
+    } else {
+        console.log(new Date(date).toUTCString)
+    }
+}
+
+class User {
+    readonly username;
+    constructor(username: string) {
+        this.username = username;
+    }
+}
+
+class Company {
+    readonly name;
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+
+function printName(entity: User | Company) {
+    if (entity instanceof User) {
+        console.log(entity.username);
+    }
+    else {
+        console.log(entity.name);
+    }
+}
+
+//////////////////////////////////////////////////////////////////
+/* Typescript allows us to write custom functions that can narrow the type of a value.
+These functions have a very special return ype called a type predicate
+
+This feature is specific to TypeScript
+
+It tells typescript if something has a certain type
+*/
+
+interface Cat {
+    name: string;
+    numLives: number;
+}
+
+interface Dog {
+    name: string;
+    breed: string;
+}
+
+// make a reusable function
+// 'pet is Cat' is our type predicate that takes the form 'parameterName is Type'!
+function isCat(pet: Cat | Dog): pet is Cat { // predicate must be our return type
+    // 'as' allows to assert it as Cat
+    return (pet as Cat).numLives !== undefined; // must reutnr a boolean
+} 
+
+function makeNoise(pet: Cat | Dog): string {
+    if (isCat(pet)){
+        return 'Meow';
+    } else {
+        return 'Woof'
+    }
+}
+
+//////////////////////////////////////////////////////////////////
+/* Discrimated Unions - a common pattern in TS involves creating a literal property that is common
+across multiple types.
+
+We can then narrow the type using that literal property
+*/
+interface Rooster {
+    name: string;
+    weight: number;
+    age: number;
+    kind: "rooster" // discriminant 
+}
+
+interface Cow {
+    name: string;
+    weight: number;
+    age: number;
+    kind: "cow" // discriminant 
+}
+
+interface Pig {
+    name: string;
+    weight: number;
+    age: number;
+    kind: "pig" // discriminant 
+}
+
+type FarmAnimal = Pig | Rooster | Cow; // type alias
+
+function getFarmAnimalSound(animal: FarmAnimal) {
+    switch(animal.kind) {
+        case('pig'):
+            return 'Oink';
+        case('cow'):
+            return 'Moo'
+        case('rooster'):
+            return 'Cockadooledoo'
+    }
+}
+
+const petuh: Rooster = {
+    name: "Petuh Chicks",
+    weight: 2,
+    age: 1.5,
+    kind: "rooster"
+}
+
+console.log(getFarmAnimalSound(petuh))
